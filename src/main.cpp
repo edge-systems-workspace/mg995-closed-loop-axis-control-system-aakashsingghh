1ddce328aa08837bd433e354d69ae6ff3670d4be
aakashsingghh <akashsingh33280@gmail.com>
2026-02-22 23:18:14 +0530

    add XML configuration files and implement servo control setup and loop

--- src/main.cpp
+++ src/main.cpp
@@ -2,22 +2,22 @@
 #include <Servo.h>
 Servo myServo;
 #define SERVO_PIN 3
-// void setup() {
-//     Serial.begin(9600);
-//     myServo.attach(SERVO_PIN);
-// }
-//
-// void loop() {
-//     myServo.write(0);
-//     delay(500);
-//     myServo.write(90);
-//     delay(500);
-//     myServo.write(120);
-//     delay(500);
-//     myServo.write(180);
-//     delay(500);
-//     myServo.write(270);
-//     delay(500);
-//
-//
-// }
+void setup() {
+    Serial.begin(9600);
+    myServo.attach(SERVO_PIN);
+}
+
+void loop() {
+    myServo.write(0);
+    delay(500);
+    myServo.write(90);
+    delay(500);
+    myServo.write(120);
+    delay(500);
+    myServo.write(180);
+    delay(500);
+    myServo.write(270);
+    delay(500);
+
+
+}
