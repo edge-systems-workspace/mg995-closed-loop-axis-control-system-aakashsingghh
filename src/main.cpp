a21506ad0ee0d6002feedbd8b0c42f8cbf22419f
aakashsingghh <akashsingh33280@gmail.com>
2026-02-22 23:17:51 +0530

    add servo control setup and loop code

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,23 @@
 #include <Arduino.h>
-void setup() {
-// write your initialization code here
-}
-
-
-void loop() {
-// write your code here
-}
+#include <Servo.h>
+Servo myServo;
+#define SERVO_PIN 3
+// void setup() {
+//     Serial.begin(9600);
+//     myServo.attach(SERVO_PIN);
+// }
+//
+// void loop() {
+//     myServo.write(0);
+//     delay(500);
+//     myServo.write(90);
+//     delay(500);
+//     myServo.write(120);
+//     delay(500);
+//     myServo.write(180);
+//     delay(500);
+//     myServo.write(270);
+//     delay(500);
+//
+//
+// }
